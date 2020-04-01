b9038c4096d41d8726598e49ea1b398b68b5bcbe
Jonasz Karwacki <jonaszakr@gmail.com>
2020-03-31 22:58:28 -0700

    add ROOT_HOSTCONF for django hosts routing

--- config/settings/base.py
+++ config/settings/base.py
@@ -307,3 +307,7 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
+
+# Django Hosts
+# ------------------------------------------------------------------------------
+ROOT_HOSTCONF = "config.hosts"
