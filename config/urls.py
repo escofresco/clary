300078526322d81e8082a61e8df38bdbcde050b0
Jonasz Karwacki <jonaszakr@gmail.com>
2020-03-31 23:48:10 -0700

    add empty urlpatterns to supress warnings

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,4 @@
+'''
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -51,3 +52,5 @@ if settings.DEBUG:
         import debug_toolbar
 
         urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+'''
+urlpatterns = []
