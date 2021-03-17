9c35ab3725fcf5b1edabb3a45bc58a601f215be5
escofresco <jonaszakr@gmail.com>
2021-03-16 18:39:03 -0700

    Remove all files

--- config/hosts.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django_hosts import patterns, host
-
-host_patterns = patterns(
-    "",
-    host(r"", "config.visitor_urls", name="visitor"),
-    host(r"dashboard", "config.member_urls", name="member"),
-)
