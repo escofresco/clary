c3420bd0775be8464a45476fcff1af4e77a4d1db
escofresco <jonaszakr@gmail.com>
2021-03-28 17:43:22 -0700

    Change main process to run preload script

--- main.js
+++ main.js
@@ -6,22 +6,26 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
     }
   })
+
   win.loadFile('static/index.html')
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
   }
 })
